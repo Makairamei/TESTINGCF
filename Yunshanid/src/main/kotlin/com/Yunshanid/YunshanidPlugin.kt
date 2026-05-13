4260668ca247d54a1acab2478d099d654dd30e30
sad25kag <sad25kag@gmail.com>
2026-05-13 12:11:08 +0700

    Create kotlin/com/Yunshanid/YunshanidPlugin.kt

---
+++ Yunshanid/src/main/kotlin/com/Yunshanid/YunshanidPlugin.kt
@@ -0,0 +1,12 @@
+package com.Yunshanid
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class YunshanidPlugin: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(YunshanidProvider())
+    }
+}
