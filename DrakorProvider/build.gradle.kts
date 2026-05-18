28b03e302910149fb01d08af1f639957c8baac74
sad25kag <sad25kag@gmail.com>
2026-05-18 07:03:39 +0700

    chore(drakor): bump extension version and update provider metadata
    - Bump extension version code to 2.
    - Define explicit metadata description, authors, and tvTypes for Drakor.
    - Set current provider status to 1 (Ok) and inject specific iconUrl source.

--- DrakorProvider/build.gradle.kts
+++ DrakorProvider/build.gradle.kts
@@ -1,10 +1,9 @@
 // use an integer for version numbers
-version = 1
+version = 2
 
 
 cloudstream {
     language = "id"
-    // All of these properties are optional, you can safely remove them
 
     description = "Drakor — Streaming Drama Korean, Movie and TV Series"
     authors = listOf("BetbetMiro")
@@ -16,13 +15,12 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
      * */
-    status = 1 // will be 3 if unspecified
+    status = 1
     tvTypes = listOf(
         "AsianDrama",
         "TvSeries",
         "Movie",
     )
-
 
     iconUrl = "https://klikxxi.me/wp-content/uploads/2024/02/cropped-site-icon.png"
 
